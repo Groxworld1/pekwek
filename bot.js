28704ba56b623fb3e8c8a9128c9f887f53bf7404
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2018-09-19 00:43:57 +0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+	if (message == 'pek'){	//If somebody says "pek", also works if previous words are space
+		bot.sendMessage({
+			to: channelID,
+			message: 'ahu'	//It'll say "ahu"
+		});
+	}
+});
