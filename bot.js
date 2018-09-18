ed95fd58a083216795d38752329aefc6890a16f9
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2018-09-19 01:33:34 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
+  bot.user.setActivity(`with ducks`);
 });
 
 bot.on("message", async message => {
@@ -11,13 +11,8 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '-';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
+  if(message.content == 'pek'){
+    message.channel.send("ahu");
   }
 });
 
