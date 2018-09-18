a1a5e73d4985030228cafebe8192a760d2c06bf9
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2018-09-19 01:23:26 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
