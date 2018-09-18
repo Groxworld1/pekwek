0dc47f7d5fb76df724f9ff255520e0e5b2afa016
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2018-09-19 01:08:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () ==> {
+	  console.log('Ready!');
+});
+
+client.on('message', () ==> {
 	  if (message.content == 'pek'){
 	  message.reply('ahu');
 	  }
