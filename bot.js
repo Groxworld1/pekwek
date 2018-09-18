fcc5b5e527abb3eede1587a89f07d9a7ab897361
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2018-09-19 01:14:02 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,29 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () ==> {
-	  console.log('Ready!');
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+	if (message == 'pek'){	//If somebody says "pek", also works if previous words are space
+		bot.sendMessage({
+			to: channelID,
+			message: 'ahu'	//It'll say "ahu"
+		});
+	}
 });
 
-client.on('message', message () ==> {
-	  if (message.content == 'pek'){
-	  	message.reply('ahu');
-	  }
-});
-	  
-client.login(process.env.BOT_TOKEN);	
+bot.login(process.env.BOT_TOKEN);
