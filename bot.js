9a23269eed8a2465769924768f2e567f29240559
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2018-10-25 16:30:16 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
+bot.on("ready", () => {
   //Tells you if DuckBot is online and running
   console.log(`${bot.user.username} is online!`);
 
   //Checks server where DuckBot is in
-  /*
   console.log("Servers to quack:");
   bot.guilds.forEach((guild) => {
     console.log("- " + guild.name);
   })
-  */
   
   //The activity
   bot.user.setActivity(`with ducks`);
