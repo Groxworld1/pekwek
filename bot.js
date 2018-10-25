5bf3a369e80d2376781646d069c68a5d65fe24a5
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2018-10-25 16:55:02 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,20 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", () => {
-  //Tells you if DuckBot is online and running
+bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-
-  //Checks server where DuckBot is in
-  console.log("Servers to quack:");
-  bot.guilds.forEach((guild) => {
-    console.log("- " + guild.name);
-  })
-  
-  //The activity
   bot.user.setActivity(`with ducks`);
 });
 
-bot.on("message", (receivedMessage) => {
-  
-  var testChannel = bot.channels.get("315142564057579521");
-  
-  /*
-  if (receivedMessage.author == bot.user){
-    return;
-  }
-  */
-  
-  /*
-  if(receivedMessage.content == "pek"){
-    receivedMessage.channel.send("ahu");
+bot.on("message", async message => {
+
+  if(message.content == 'pek'){
+    message.channel.send("ahu");
   }
   
-  else if(receivedMessage.content == "ahu"){
-    receivedMessage.channel.send("pek");
+  else if(message.content == 'ahu'){
+    message.channel.send("pek");
   }
-  */
-    
-  // receivedMessage.channel.send("Message received " + receivedMessage.content);
 });
 
 bot.login(process.env.token);
